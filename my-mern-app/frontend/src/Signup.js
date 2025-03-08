4e171250db985baabe507c5fa659ef13879b018c
username <himashachinthani@gmail.com>
2025-03-08 10:41:40 +0530

    add css connect correctly

--- my-mern-app/frontend/src/Signup.js
+++ my-mern-app/frontend/src/Signup.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import './Signup.css'; 
+import './css/Signup.css';
+
 
 const Signup = () => {
   const [formData, setFormData] = useState({ username: '', password: '', confirmPassword: '' });
